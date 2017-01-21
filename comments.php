9e09c6a291759c062df8415659e31e5517db911e
Ethan Choi <ethanchoi.812@gmail.com>
2017-01-21 23:38:30 +0800

    Comments, page, archive

---
+++ comments.php
@@ -0,0 +1,34 @@
+<div class="comments">
+	<h2>Comments</h2>
+	<?php
+		$args = array(
+        'walker'            => null,
+        'max_depth'         => '',
+        'style'             => 'ul',
+        'callback'          => null,
+        'end-callback'      => null,
+        'type'              => 'all',
+        'reply_text'		=> 'Reply',
+        'page'              => '',
+        'per_page'          => '',
+        'avatar_size'       => 80,
+        'reverse_top_level' => null,
+        'reverse_children'  => '',
+        'format'            => current_theme_supports( 'html5', 'comment-list' ) ? 'html5' : 'xhtml',
+        'short_ping'        => false,
+        'echo'              => true,
+    ); ?>
+
+    <?php wp_list_comments($args, $comments); ?>
+
+    <?php
+    $form_args = array(
+    	'label_submit' => 'Send',
+    	'title_reply'          => 'Leave a Reply',
+    	'comment_notes_after'  => '',
+    	'comment_field'        => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
+    	);
+
+    comment_form($form_args);
+    	?>
+</div>
